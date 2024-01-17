5b7caf1c6c18b5c145fc741db5e9bb672045279f
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2024-01-17 21:50:49 +0000

    MYSQL

--- 0x00-MySQL_Advanced0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,12 @@
--- File: task0
--- Task: Write a SQL script that creates a table users
+-- File: create_users_table.sql
+-- Task: Create the 'users' table with specified attributes.
 
 -- If the table already exists, the script should not fail.
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255) 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
+
+-- Additional comment: 
 -- Making the 'email' attribute unique directly in the table schema enforces business rules and avoids application bugs.
