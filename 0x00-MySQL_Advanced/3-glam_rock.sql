e04a9d65ee346657c9d4c96fb5b2e4498792675b
Adeosun Oluwaseyi Esther <deosundeol@gmail.com>
2024-01-18 11:27:29 +0100

    MYSQL

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- File: Task 3
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+
+SELECT band_name, (2022 - formed) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
