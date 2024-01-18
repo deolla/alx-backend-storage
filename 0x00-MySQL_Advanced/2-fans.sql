d18095e52cf89bd0d59b64c0b1bfb57179eede47
Adeosun Oluwaseyi Esther <deosundeol@gmail.com>
2024-01-18 10:58:16 +0100

    MYSQL

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,18 +1,7 @@
 -- File: Task 2
 -- Task: Rank country orgins of band ordered by number of (non-unique) fans
 
--- Create a temporary table to store the rankings
-CREATE TEMPORARY TABLE temp_rankings AS (
-    SELECT origin, COUNT(*) AS nb_fans
-    FROM metal_bands
-    GROUP BY origin
-);
-
--- Rank the countries based on the number of fans in descending order
-SET @rank = 0;
-SELECT origin, nb_fans, @rank := @rank + 1 AS rank
-FROM temp_rankings
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
 ORDER BY nb_fans DESC;
-
--- Drop the temporary table
-DROP TEMPORARY TABLE IF EXISTS temp_rankings;
