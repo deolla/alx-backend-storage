260e2acff905bc41bd86321a73e7854acb02b7aa
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-23 10:58:12 -0800

    mongodb

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
-"""Python script that provides stats about Nginx logs stored in MongoDB"""
+"""Add top 10 of the most present IPs in collection nginx of database logs:"""
 import pymongo
 
 if __name__ == "__main__":
     client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
-    
+
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
-    
+
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
-        
+
     status_check_count = collection.count_documents({
         "method": "GET", "path": "/status"
     })
-        
+
     print(f"{status_check_count} status check")
-    
+
     ids = collection.aggregate([
         {"$group":
             {
                 "_id": "$ip",
                 "count": {"$sum": 1}
             }
-        },
+         },
         {"$sort": {"count": -1}},
         {"$limit": 10},
         {"$project": {
