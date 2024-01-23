58e9de59cc65c4e7e2d6962a480a53544145e6e3
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-23 05:14:32 -0800

    mongodb

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a Python function that lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """lists all documents in a collection"""
+    docs = []
+    results = mongo_collection.find()
+    for i in results:
+        docs.append(i)
+    if len(docs) == 0:
+        return []
+    return docs
