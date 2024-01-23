5c3e2b33b498c75f0842536cf7f916067d534f1a
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-23 07:48:06 -0800

    mongodb

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -15,13 +15,17 @@ print(f"{total_logs} logs")
 
 # Get the count of different methods
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {method: collection.count_documents({"method": method}) for method in methods}
+method_counts = {method: collection.count_documents({
+    "method": method
+}) for method in methods}
 
 print("Methods:")
 for method, count in method_counts.items():
     print(f"    method {method}: {count}")
 
 # Get the count of logs with method=GET and path=/status
-status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+status_check_count = collection.count_documents({
+    "method": "GET", "path": "/status"
+})
 
 print(f"{status_check_count} status check")
