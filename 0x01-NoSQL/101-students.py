44b67c71baa7ab171d5a5d2261c8bb59af370cd8
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-23 11:03:29 -0800

    mongodb

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
 """A Python function that returns all students sorted by average score"""
-
+import operator
 
 def top_students(mongo_collection):
-    students = mongo_collection.find()
-    student_scores = []
-    
-    for student in students:
-        if 'scores' in student:
-            scores = student['scores']
-            average_score = sum(score['score'] for score in scores) / len(scores)
-            student_scores.append((student['_id'], student['name'], scores, average_score))
-    sorted_students = sorted(student_scores, key=lambda x: x[3], reverse=True)
-    
-    for student in sorted_students:
-        student_id = student[0]
-        student_name = student[1]
-        scores = student[2]
-        average_score = student[3]
-        print(f"[{student_id}] {student_name} - {scores}")
-    
-    for student in sorted_students:
-        student_id = student[0]
-        student_name = student[1]
-        average_score = student[3]
-        print(f"[{student_id}] {student_name} => {average_score}")
-    return sorted_students
+    """Returns all students sorted by average score"""
+    students = []
+
+    coll = mongo_collection.find()
+    for document in coll:
+        if "scores" not in document:
+            continue
+
+        scores = document["scores"]
+
+        if not isinstance(scores, list):
+            continue
+
+        if len(scores) == 0:
+            continue
+
+        average_score = sum(score["score"] for score in scores) / len(scores)
+
+        student_info = {
+            "name": document["name"],
+            "averageScore": average_score
+        }
+
+        students.append(student_info)
+
+    students.sort(key=operator.itemgetter("averageScore"), reverse=True)
+
+    sorted_student_names = [student["name"] for student in students]
+    return sorted_student_names
