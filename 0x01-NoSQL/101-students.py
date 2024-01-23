cc62fa90a54ed120ceb0098f7d40c173c4a14c9f
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-23 08:25:45 -0800

    mongodb

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""A Python function that returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    # Find all documents in the collection
+    students = mongo_collection.find()
+
+    # Calculate the average score for each student
+    for student in students:
+        scores = student['topics']
+        total_score = sum(score['score'] for score in scores)
+        average_score = total_score / len(scores)
+        student['averageScore'] = average_score
+
+    # Sort the students by average score in descending order
+    sorted_students = sorted(students, key=lambda x: x['averageScore'], reverse=True)
+
+    # Print the sorted students and their scores
+    for student in sorted_students:
+        print("[{}] {} - {}".format(student['_id'], student['name'], student['topics']))
+
+    # Return the sorted students
+    return sorted_students
