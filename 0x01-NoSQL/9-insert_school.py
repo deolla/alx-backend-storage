ead0d2e35e03df45c98afaa04e45b88aa04ee694
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-23 05:28:50 -0800

    mongodb

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/ python3
+"""A Python function that inserts new document in collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection"""
+    docs = {}
+    for key, value in kwargs.items():
+        docs[key] = value
+    ids = mongo_collection.insert_one(docs)
+    new_ids = ids.inserted_id
+    return new_ids
