113aab752f50cbaf743f6bc4e196f1174c8f623d
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-24 19:48:14 +0100

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,33 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
+"""Implement a get_page function"""
+
 import requests
+import redis
 from functools import wraps
 from typing import Callable
 
-
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+r = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def count_requests(method: Callable) -> Callable:
+    """Count the number of requests"""
+
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):
+        """Wrapper function for decorator"""
+        r.incr(f"count:{url}")
+        caches = r.get(f"cached:{url}")
+        if caches:
+            return caches.decode("utf-8")
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
 
 
-@data_cacher
+@count_requests
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """Get the HTML content of a particular URL and returns it"""
+    req = requests.get(url)
+    return req.text
