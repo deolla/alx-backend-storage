b3ada88ec435ce901ee96c657cd39c020812da1a
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-24 19:28:34 +0100

    redis

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Implement a get_page function"""
+
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+
+r = redis.Redis()
+
+
+def count_requests(method: Callable) -> Callable:
+    """Count the number of requests"""
+
+    @wraps(method)
+    def wrapper(url):
+        """Wrapper function for decorator"""
+        r.incr(f"count:{url}")
+        caches = r.get(f"cached:{url}")
+        if caches:
+            return caches.decode("utf-8")
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
+def get_page(url: str) -> str:
+    """Get the HTML content of a particular URL and returns it"""
+    req = requests.get(url)
+    return req.text
